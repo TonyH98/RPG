26dacbf2c882727d38abbaea8201323fd60f829b
TonyH98 <tonyhoang@pursuit.org>
2024-08-14 14:55:53 -0400

    Basic player movement added

--- src/App.java
+++ src/App.java
@@ -20,5 +20,7 @@ public class App {
 
        window.setVisible(true);
 
+       gamePanel.startGameThread();
+
     }
 };
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 import javax.swing.plaf.DimensionUIResource;
 
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
 
 //Screen Settings 
 
@@ -20,11 +22,101 @@ final int maxScreenRow = 12;
 final int screenWidth = titleSize * maxScreenCol; //768 pixels
 final int screenHeight = titleSize * maxScreenRow; // 576 pixels;
 
+//fps
+int fps = 60;
+
+KeyHandler keyH = new KeyHandler();
+
+Thread gameThread;  
+
+int plyayerX = 100; 
+int playerY = 100; 
+int playerSpeed = 4; 
 
 public GamePanel(){
 
     this.setPreferredSize(new DimensionUIResource(screenWidth, screenHeight));
     this.setBackground(Color.BLACK);
     this.setDoubleBuffered(true);
+    this.addKeyListener(keyH);
+    this.setFocusable(true);
 }
+
+public void startGameThread(){
+    gameThread = new Thread(this);
+    gameThread.start();
+}
+
+@Override
+public void run(){
+
+    double drawInterval = 1000000000/fps; //draw the graphics every 0.0166 seconds
+
+    double nextDrawTime = System.nanoTime() + drawInterval;
+
+
+    while(gameThread != null){
+
+        //Update information such as character position 
+        update();
+
+        //Draw the screen with the udapted information
+        repaint();
+
+        
+        try{
+            double remainingTime = nextDrawTime - System.nanoTime(); //Check the next update 
+
+            remainingTime = remainingTime/1000000;
+
+            if(remainingTime < 0){
+
+                remainingTime = 0;
+            }
+
+            Thread.sleep((long)remainingTime); //pause the game loop until the sleep time is over
+
+            nextDrawTime += drawInterval;
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
+public void update(){
+
+    if(keyH.upPressed == true){
+        playerY -= playerSpeed;
+        playerY = playerY - playerSpeed;
+    }
+    else if(keyH.downPressed == true){
+        playerY += playerSpeed;
+
+    }
+    else if(keyH.leftPressed == true){
+        plyayerX -= playerSpeed;
+    }
+    else if(keyH.rightPressed == true){
+        plyayerX += playerSpeed;
+    }
+
+}
+
+
+public void paintComponent(Graphics g){
+
+    super.paintComponent(g); 
+
+    Graphics2D g2 = (Graphics2D)g;
+
+    g2.setColor(Color.white);
+
+    g2.fillRect(plyayerX, playerY, titleSize, titleSize);
+
+    g2.dispose();
+
+}
+
 }
